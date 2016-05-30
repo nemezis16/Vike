883f022e121bac0b70de892c06e1bebea96301e8
Nemezis16 <nemezis16@gmail.com>
2016-05-30 15:39:35 +0300

    Some changes

--- VikeTestProject/Vike/Classes/VikeUncaughtExceptionHandler.m
+++ VikeTestProject/Vike/Classes/VikeUncaughtExceptionHandler.m
@@ -60,7 +60,7 @@ void CompleteHandledException(NSException *exception)
                                           @"name" : exception.name,
                                           @"stacktrace" : stacktraceString,
                                           @"fatal" : @1};
-    BOOL complete;
+    BOOL complete = NO;
     
     if (selfRef.exceptionCallback) {
         selfRef.exceptionCallback(exceptionDictionary, &complete);
